c46b9151e1af3d4f2060bc636ae0351f93b6ea81
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-18 10:15:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(a,b){
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(a,b){
+  return drivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(valueMultiplier){
+  return function(fareMultiplier){
+    return valueMultiplier * fareMultiplier;
+  }
+  }
+
+
+  const fareDoubler = createFareMultiplier(2)
+  createFareMultiplier()
+
+  const fareTripler = createFareMultiplier(3)
+
+
+  function selectDifferentDrivers(drivers, returnLastTwoDrivers){
+    return returnLastTwoDrivers(drivers)
+  }
+
+  
